39107541b797d1c0b02db765f7c6b64b753c90db
hamiceis <hamiceis@hotmail.com>
2023-03-03 13:23:02 -0300

    alteração button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,7 +5,7 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 export function Button({ title, ...props }: ButtonProps) {
   return (
     <button
-      className=" w-60 p-1 bg-black rounded-lg hover:opacity-70 transition-transform focus:animate-pulse"
+      className="w-56 p-1 ml-7 bg-black rounded-lg hover:opacity-70 transition-transform focus:animate-pulse"
       {...props}
     >
       {title}
